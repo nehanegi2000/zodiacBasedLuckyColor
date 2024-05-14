67be53407d1b699b46aed8bc1cddb3cca62e5c2b
nehanegi2000 <51457816+nehanegi2000@users.noreply.github.com>
2024-05-14 20:49:36 +0530

    Add files via upload

---
+++ zodiac.js
@@ -0,0 +1,57 @@
+let changeColor=()=>
+    { 
+        let input= document.getElementById("zodiac");
+        console.log(input.value);
+
+  if(input.value=="aries")
+    {
+        document.body.style.backgroundColor="pink";
+    }
+    if(input.value=="tarus")
+        {
+            document.body.style.backgroundColor="red";
+        }
+        if(input.value =="gemini")
+            {
+                document.body.style.backgroundColor="green";
+            }
+            if(input.value =="cancer")
+                {
+                    document.body.style.backgroundColor="blue";
+                }
+                if(input.value =="leo")
+                    {
+                        document.body.style.backgroundColor="skyblue";
+                    }
+                    if(input.value =="virgo")
+                        {
+                            document.body.style.backgroundColor="yellow";
+                        }
+                        if(input.value =="libra")
+                            {
+                                document.body.style.backgroundColor="lightgreen";
+                            }
+                            if(input.value =="scorpio")
+                                {
+                                    document.body.style.backgroundColor="aqua";
+                                }
+                                if(input.value =="sagittarius")
+                                    {
+                                        document.body.style.backgroundColor="orange";
+                                    }
+                                    if(input.value =="capricorn")
+                                        {
+                                            document.body.style.backgroundColor="magenta";
+                                        }
+                                        if(input.value =="aquarius")
+                                            {
+                                                document.body.style.backgroundColor="black";
+                                            }
+                                            if(input.value =="pisces")
+                                                {
+                                                    document.body.style.backgroundColor="white";
+                                                }
+    
+                    
+                                            }
+                    
